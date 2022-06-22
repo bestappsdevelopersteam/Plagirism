ec4b3eb4fb219b7fac3a992bdb4972b54f6cc7ef
bestappsdevelopersteam <bestappsdevelopersteam@gmail.com>
2022-06-22 11:38:15 +0300

    plagiarismDetector

--- src/plagirismDetector.java
+++ src/plagirismDetector.java
@@ -4,18 +4,18 @@ import java.util.Arrays;
 import java.util.List;
 
 public class plagirismDetector {
-    public static void main(String args[]) {
+    public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Please enter first text and press ENTER : ");
         String textOne = sc.nextLine();
         textOne = textOne.toLowerCase();
-        List<String> firstTextWords = new ArrayList<String>(Arrays.asList(textOne.split("([,.!?_;=+-:\\s]+)")));
+        List<String> firstTextWords = new ArrayList<>(Arrays.asList(textOne.split("([,.!?_;=+-:\\s]+)")));
         for (int i = 0; i < firstTextWords.size(); i++) {
 //            System.out.println(firstTextWords.get(i));
         }
         int firstTextWordsCount = firstTextWords.size();
 //        System.out.println(firstTextWordsCount);
-        List<String> firstTextSentence = new ArrayList<String>(Arrays.asList(textOne.split("([.!?;:]+)")));
+        List<String> firstTextSentence = new ArrayList<>(Arrays.asList(textOne.split("([.!?;:]+)")));
         for (int i = 0; i < firstTextSentence.size(); i++) {
 //            System.out.println(firstTextSentence.get(i));
         }
@@ -25,13 +25,13 @@ public class plagirismDetector {
         System.out.println("Please enter second text and press ENTER : ");
         String textTwo = sc.nextLine();
         textTwo = textTwo.toLowerCase();
-        List<String> secondTextWords = new ArrayList<String>(Arrays.asList(textTwo.split("([,.!?_;=+-:\\s]+)")));
+        List<String> secondTextWords = new ArrayList<>(Arrays.asList(textTwo.split("([,.!?_;=+-:\\s]+)")));
         for (int j = 0; j < secondTextWords.size(); j++) {
 //            System.out.println(secondTextWords.get(j));
         }
         int secondTextWordsCount = secondTextWords.size();
 //        System.out.println(secondTextWordsCount);
-        List<String> secondTextSentence = new ArrayList<String>(Arrays.asList(textTwo.split("([.!?;:]+)")));
+        List<String> secondTextSentence = new ArrayList<>(Arrays.asList(textTwo.split("([.!?;:]+)")));
         for (int j = 0; j < secondTextSentence.size(); j++) {
 //            System.out.println(secondTextSentence.get(j));
         }
@@ -40,22 +40,23 @@ public class plagirismDetector {
 //        ..................................
 
         countAverageLengthElementOfText(firstTextWords);
-        System.out.println(countAverageLengthElementOfText(firstTextWords)); // here not return averagelengthof element, but return list size?!(Why)
+        //    System.out.println(countAverageLengthElementOfText(firstTextWords)); // return averageLengthEachElement
     }
 
 
     public static double countAverageLengthElementOfText(List<String> array) {
         double arSize = array.size();
-        double lengthEachElement=0;
-        double sumLengthEachElement=0;
-        double averageLengthEachElement=0;
-        for (int i = 0; i < arSize; i++) {
-            lengthEachElement = array.get(i).length();
-            sumLengthEachElement+=lengthEachElement;
-
+        double lengthEachElement = 0;
+        double sumLengthEachElement = 0;
+        double averageLengthEachElement = 0;
+        for (int i = 0; i < arSize; i++) {               //for (String s : array) {
+            lengthEachElement = array.get(i).length();   //lengthEachElement = s.length();
+            sumLengthEachElement += lengthEachElement;
         }
-        averageLengthEachElement=sumLengthEachElement/arSize;
+        averageLengthEachElement = sumLengthEachElement / arSize;
         return averageLengthEachElement;
+
+    }
     }
 
 
