2062044e006326787896e0a24200309f6fa0780b
bestappsdevelopersteam <bestappsdevelopersteam@gmail.com>
2022-06-23 11:07:10 +0300

    plagiarismDetector

--- src/plagirismDetector.java
+++ src/plagirismDetector.java
@@ -6,23 +6,30 @@ public class plagirismDetector { //different name  with mistake (plagirism)for h
         System.out.println("Please enter first text and press ENTER : ");
         String textOne = sc.nextLine();
         textOne = textOne.toLowerCase();
-        List<String> firstTextWords = new ArrayList<>(Arrays.asList(textOne.split("([,.!?_;=+-:\\s]+)"))); //read words in array
-        for (int i = 0; i < firstTextWords.size(); i++) {
-//            System.out.println(firstTextWords.get(i));  //TEST . To check correction.
-        }
-        double firstTextWordsCount = firstTextWords.size();
-//        System.out.println(firstTextWordsCount);        //TEST . To check correction.
-        List<String> firstTextSentence = new ArrayList<>(Arrays.asList(textOne.split("([.!?;:]+)")));//read sentence in array
-        for (int i = 0; i < firstTextSentence.size(); i++) {
-//            System.out.println(firstTextSentence.get(i));    //TEST . To check correction.
-        }
-        double firstTextSentenceCount = firstTextSentence.size();
+        List<String> firstTextWords = new ArrayList<>(Arrays.asList(textOne.split("([,.!?_;=+-:()\\s]+)"))); //read words in array -use punctuation divider
+//        for (int i = 0; i < firstTextWords.size(); i++) {   //TEST . To check correction of read words.
+//            System.out.print(firstTextWords.get(i) + " ");  //TEST . To check correction of read words.
+//        }                                                  //TEST . To check correction of read words.
+//            System.out.println();                              //TEST . To check correction of read words.
+        double firstTextWordsCount = firstTextWords.size();     //declare var to be "double"
+//            System.out.println(firstTextWordsCount);        //TEST . To check correction - > count words in text.
+        List<String> firstTextSentence = new ArrayList<>(Arrays.asList(textOne.split("([.!?;:]+)")));//read sentence in array -use punctuation divider
+
+//        for (int j = 0; j < firstTextSentence.size(); j++) { //TEST . To check correction - > count sentence in text.
+//            System.out.println(firstTextSentence.get(i));    //TEST . To check correction - > count sentence in text.
+//        }                                                    //TEST . To check correction - > count sentence in text.
+        double firstTextSentenceCount = firstTextSentence.size();//declare var to be "double"
 //        System.out.println(firstTextSentenceCount);       //TEST . To check correction.
+//-----------------------Next is print statement features of first text on console ....................................................
+        System.out.println("1. Avg. word length:     " + countAverageLengthElementOfText(firstTextWords)); // return averageLengthEachElement(in our case words)
+        System.out.println("2. Type-Token Ratio:     " + typeTokenRation(firstTextWords)); // return count of unique words divided on all words form inputted text.
+        System.out.println("3. Hapax Legomena Ratio: " + HapaxLegomenaRatio(firstTextWords)); // return count of only one met word divided on all words form inputted text.
+        System.out.println("4. Avg. sentence length: " + calcAverageWordsInSentence(firstTextWordsCount, firstTextSentenceCount)); // return average number words in sentence.
 
         System.out.println("Please enter second text and press ENTER : ");
         String textTwo = sc.nextLine();
         textTwo = textTwo.toLowerCase();
-        List<String> secondTextWords = new ArrayList<>(Arrays.asList(textTwo.split("([,.!?_;=+-:\\s]+)")));
+        List<String> secondTextWords = new ArrayList<>(Arrays.asList(textTwo.split("([,.!?_;=+-:()\\s]+)")));
         for (int j = 0; j < secondTextWords.size(); j++) {
 //            System.out.println(secondTextWords.get(j));     //TEST . To check correction.
         }
@@ -32,26 +39,17 @@ public class plagirismDetector { //different name  with mistake (plagirism)for h
         for (int j = 0; j < secondTextSentence.size(); j++) {
 //            System.out.println(secondTextSentence.get(j));    //TEST . To check correction.
         }
-        double secondTextSentenceCount = secondTextSentence.size();
-//        System.out.println(secondTextSentenceCount);    //TEST . To check correction.
+        double secondTextSentenceCount = secondTextSentence.size();//declare var to be "double"
+//        System.outT.println(firstTextSentenceCount);       //TEST . To check correction.
+//-----------------------Next is print statement features of first text on console ....................................................
+        System.out.println("1. Avg. word length:     " + countAverageLengthElementOfText(secondTextWords)); // return averageLengthEachElement(in our case words)
+        System.out.println("2. Type-Token Ratio:     " + typeTokenRation(secondTextWords)); // return count of unique words divided on all words form inputted text.
+        System.out.println("3. Hapax Legomena Ratio: " + HapaxLegomenaRatio(secondTextWords)); // return count of only one met word divided on all words form inputted text.
+        System.out.println("4. Avg. sentence length: " + calcAverageWordsInSentence(secondTextWordsCount, secondTextSentenceCount)); // return average number words in sentence.
 
-        countAverageLengthElementOfText(firstTextWords);
-//        System.out.println(countAverageLengthElementOfText(firstTextWords)); // return averageLengthEachElement(in our case words)
-        countAverageLengthElementOfText(secondTextWords);
-
-        typeTokenRation(firstTextWords);
-//        System.out.println(typeTokenRation(firstTextWords)); // return count of unique words divided on all words form inputted text.
-        typeTokenRation(secondTextWords);
-
-        HapaxLegomenaRatio(firstTextWords);
-        HapaxLegomenaRatio(secondTextWords);
-        System.out.println(HapaxLegomenaRatio(firstTextWords)); // return count of only one met word divided on all words form inputted text.
-        calcAverageWordsInSentence(firstTextWordsCount, firstTextSentenceCount);
-        System.out.println(calcAverageWordsInSentence(firstTextWordsCount, firstTextSentenceCount)); // return average number words in sentence.
-        calcAverageWordsInSentence(secondTextWordsCount, secondTextSentenceCount);
     }
 
-    //        ..................................Some methods below.......................................
+    //        ..................................features methods below.......................................
     public static double countAverageLengthElementOfText(List<String> array) {// method return averageLengthEachElement(in our case words)
         double arSize = array.size();
         double lengthEachElement = 0;
